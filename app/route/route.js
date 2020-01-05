9df793b01b8c9740206a18ca0177a20e405ab23c
Sagar Shankhala <sagshank@publicisgroupe.net>
2020-01-05 17:56:56 +0530

    changed readme file

--- app/route/route.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const Book = require("../controller/BookController");
-    app.get("/", (req, res) => {
-        res.send({"message": "welcome in my cls"});
-    });
-    app.get("/book/:id", Book.Find);
-    app.post("/book/save", Book.Save);
-    app.delete("/book/delete/:id", Book.Delete);
-};
